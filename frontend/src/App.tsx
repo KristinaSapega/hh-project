5c5aaada08cff232f79a9ae41d4ca716962a9e16
SKornya <kornyakovS@gmail.com>
2024-03-24 18:15:52 +0300

    смена тем, контейнер для приложения

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,54 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ThemeProvider } from '@emotion/react';
+import { createTheme, CssBaseline, Box } from '@mui/material';
+
+import Header from './components/Header';
 
 import { routes } from './routes/routes';
 
 const App = () => {
+  // для смены темы. в палитре заданы свои основные цвета тем
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+  const theme = createTheme({
+    palette: {
+      mode,
+      primary: {
+        main: '#9a9a9a',
+      },
+      secondary: {
+        main: '#9a9a9a',
+      },
+    },
+  });
+
+  // функция для переключения темы, передаем пропсом в хедер
+  const toggleTheme = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route path={routes.main} element={<></>} />
-          <Route path={routes.login} element={<></>} />
-          <Route path={routes.stand} element={<></>} />
-          <Route path="*" element={<></>} />
-        </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Box
+          sx={{
+            bgcolor: 'background.default',
+            paddingTop: '80px',
+            height: '100vh',
+          }}
+        >
+          <Header toggleTheme={toggleTheme} mode={mode} />
+          <Routes>
+            <Route path={routes.main} element={<></>} />
+            <Route path={routes.login} element={<></>} />
+            <Route path={routes.stand} element={<></>} />
+            <Route path="*" element={<></>} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
