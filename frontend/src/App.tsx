56d6d76c3dcc5ace89e7439d566874219bacf78f
SKornya <kornyakovS@gmail.com>
2024-03-24 17:45:22 +0300

    add routing components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { routes } from './routes/routes';
+
 const App = () => {
-  return (<></>)
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path={routes.main} element={<></>} />
+          <Route path={routes.login} element={<></>} />
+          <Route path={routes.stand} element={<></>} />
+          <Route path="*" element={<></>} />
+        </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
