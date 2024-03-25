449b0a514a83278c4d21a28a0982d2b5f4cb0bd0
Kom <masha.komendantova@yandex.ru>
2024-03-25 09:00:55 +0300

    initial back repo

---
+++ backend/src/test/java/com/example/crab/CrabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
