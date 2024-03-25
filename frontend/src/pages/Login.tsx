a89619fb2ca49cf33bb7147ab6ee461a9be72f09
SKornya <kornyakovS@gmail.com>
2024-03-25 18:52:15 +0300

    login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import { Container } from '@mui/material';
+import LoginForm from '../components/Login/LoginForm';
+import SignupForm from '../components/Login/SignupForm';
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+
+  const formSwitch = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+      }}
+    >
+      {isLogin ? (
+        <LoginForm formSwitch={formSwitch} />
+      ) : (
+        <SignupForm formSwitch={formSwitch} />
+      )}
+    </Container>
+  );
+};
+
+export default Login;
