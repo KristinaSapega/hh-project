abe0ea76558543ac61b8f6cb002c53c2205a0b6a
Kom <masha.komendantova@yandex.ru>
2024-04-16 09:56:58 +0300

    [back-21] fix standrepository

--- backend/src/main/java/com/example/crab/persistence/StandRepository.java
+++ backend/src/main/java/com/example/crab/persistence/StandRepository.java
@@ -4,6 +4,6 @@ import com.example.crab.entity.StandEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StandRepository extends JpaRepository<StandEntity, Long> {
-  Optional<StandEntity> findById(Integer id);
+public interface StandRepository extends JpaRepository<StandEntity, Integer> {
+
 }
