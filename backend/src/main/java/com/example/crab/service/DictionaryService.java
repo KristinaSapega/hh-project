4a8e533df3bf5d8958970995a8e24127c30c7064
Kom <masha.komendantova@yandex.ru>
2024-04-18 14:12:40 +0300

    [back-25] mock controller dictionary

---
+++ backend/src/main/java/com/example/crab/service/DictionaryService.java
@@ -0,0 +1,20 @@
+package com.example.crab.service;
+
+import com.example.crab.transport.dictionary.Dictionary;
+import com.example.crab.transport.dictionary.DictionaryList;
+import com.example.crab.transport.dictionary.Field;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DictionaryService {
+  public DictionaryList getDictionaries() {
+    Dictionary dictionary = new Dictionary(1, "DeployService", "Клонирование ветки на стенд",
+        List.of(
+            new Field("input","repo","repo url"),
+            new Field("input","repoOwner","repo owner"),
+            new Field("input","branch","repo branch")
+        ));
+    return new DictionaryList(List.of(dictionary));
+  }
+}
---
+++ backend/src/main/java/com/example/crab/transport/dictionary/Dictionary.java
@@ -0,0 +1,15 @@
+package com.example.crab.transport.dictionary;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+public record Dictionary(
+    @Schema(description = "id playbook", example = "12")
+    Integer id,
+    @Schema(description = "Название playbook", example = "DeployService")
+    String type,
+    @Schema(description = "Описание playbook", example = "Клонирование ветки на стенд")
+    String description,
+    @Schema(description = "Описание полей")
+    List<Field> fields) {
+}
