18d13351f9ef1a70eba04c48a9d09a2e6f7f0c5a
SKornya <kornyakovS@gmail.com>
2024-04-20 22:34:54 +0300

    stand id instead stand host

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -140,7 +140,7 @@ export interface RemovePluginAction {
 // TasksReducer
 
 export interface TasksState {
-  stands: Array<string>;
+  stands: Array<number>;
   tasks: Array<{
     id: number;
     type: string;
@@ -152,12 +152,12 @@ export interface TasksState {
 
 export interface AddStandToTasksAction {
   type: string;
-  payload: string;
+  payload: number;
 }
 
 export interface RemoveStandFromTasksAction {
   type: string;
-  payload: string;
+  payload: number;
 }
 
 export interface AddTaskToTasksAction {
