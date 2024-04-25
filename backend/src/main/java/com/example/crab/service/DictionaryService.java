971b06765dc5cd17882c90af54f64df3144cb734
Kom <masha.komendantova@yandex.ru>
2024-04-25 19:33:23 +0300

    [back-32] edit securityconfig and rename dictionary

--- backend/src/main/java/com/example/crab/service/DictionaryService.java
+++ backend/src/main/java/com/example/crab/service/DictionaryService.java
@@ -1,6 +1,6 @@
 package com.example.crab.service;
 
-import com.example.crab.transport.dictionary.Dictionary;
+import com.example.crab.transport.dictionary.Task;
 import com.example.crab.transport.dictionary.DictionaryList;
 import com.example.crab.transport.dictionary.Field;
 import java.util.List;
@@ -9,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class DictionaryService {
   public DictionaryList getDictionaries() {
-    Dictionary dictionary = new Dictionary(1, "DeployService", "Клонирование ветки на стенд",
+    Task task = new Task(1, "DeployService", "Клонирование ветки на стенд",
         List.of(
             new Field("input","repo","name repo"),
             new Field("input","repoOwner","repo owner"),
             new Field("input","branch","repo branch")
         ));
-    return new DictionaryList(List.of(dictionary));
+    return new DictionaryList(List.of(task));
   }
 }
