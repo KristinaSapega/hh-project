996c7dc7117b581556f759bb2bc05dffd58b13b3
SKornya <kornyakovS@gmail.com>
2024-04-25 17:20:56 +0300

    dark theme by default

--- frontend/src/components/Stands.tsx
+++ frontend/src/components/Stands.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { AddOutlined } from '@mui/icons-material';
@@ -6,6 +7,11 @@ import { Tooltip, useTheme } from '@mui/material';
 import Button from '@mui/material/Button';
 import { DataGrid, GridCellParams, GridColDef } from '@mui/x-data-grid';
 
+import { useAppDispatch } from '../hooks/useAppDispatch';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { takeStand } from '../reducers/standsReducer';
+import { BASE_BACKEND_URL, routes } from '../routes/routes';
+import { RootState } from '../store';
 import { Stand } from '../types';
 
 const RenderButtonCell: FunctionComponent<{ params: GridCellParams }> = ({
@@ -13,19 +19,52 @@ const RenderButtonCell: FunctionComponent<{ params: GridCellParams }> = ({
 }) => {
   const standId = params.row.id;
 
+  const dispatch = useAppDispatch();
+  const { user } = useAuthContext();
+
+  const stand = useSelector((state: RootState) => state.stands.stands).find(
+    ({ id }) => id === standId,
+  ) as Stand;
+
+  const handleTakeStand = async () => {
+    try {
+      const response = await fetch(routes.api.stand(standId), {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Basic ${user}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ takenBy: 'admin@test.ru' }),
+      });
+
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+
+      dispatch(takeStand('admin@test.ru', standId));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <Link to={`/stand/${standId}`}>
-      <Button
-        variant="contained"
-        color="success"
-        sx={{
-          borderRadius: '10px',
-          boxShadow: 'none',
-        }}
-      >
-        <AddOutlined />
-      </Button>
-    </Link>
+    <>
+      {!stand.takenBy && (
+        <Link to={`/stand/${standId}`}>
+          <Button
+            variant="contained"
+            color="success"
+            sx={{
+              borderRadius: '10px',
+              boxShadow: 'none',
+            }}
+            onClick={handleTakeStand}
+          >
+            <AddOutlined />
+          </Button>
+        </Link>
+      )}
+    </>
   );
 };
 
