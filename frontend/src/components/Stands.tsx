c8830e96cfca84a658d9f50206d2d828d6248e90
SKornya <kornyakovS@gmail.com>
2024-04-26 23:36:21 +0300

    modal is closing after stand action, switch button to icon

--- frontend/src/components/Stands.tsx
+++ frontend/src/components/Stands.tsx
@@ -1,76 +1,71 @@
-import { FunctionComponent } from 'react';
+import { FC } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
-import { AddOutlined } from '@mui/icons-material';
-import { Tooltip, useTheme } from '@mui/material';
-import Button from '@mui/material/Button';
+import { LoginOutlined, LogoutOutlined } from '@mui/icons-material';
+import { IconButton, Tooltip, useTheme } from '@mui/material';
 import { DataGrid, GridCellParams, GridColDef } from '@mui/x-data-grid';
 
 import { useAppDispatch } from '../hooks/useAppDispatch';
 import { useAuthContext } from '../hooks/useAuthContext';
-import { takeStand } from '../reducers/standsReducer';
-import { BASE_BACKEND_URL, routes } from '../routes/routes';
 import { RootState } from '../store';
+import { apiLeaveStand, apiTakeStand } from '../store/stands';
 import { Stand } from '../types';
 
-const RenderButtonCell: FunctionComponent<{ params: GridCellParams }> = ({
+const RenderButtonCell: FC<{ params: GridCellParams; onClose: () => void }> = ({
   params,
+  onClose,
 }) => {
+  const { user } = useAuthContext();
+  const login = atob(user!).split(':')[0];
+
   const standId = params.row.id;
 
   const dispatch = useAppDispatch();
-  const { user } = useAuthContext();
 
   const stand = useSelector((state: RootState) => state.stands.stands).find(
     ({ id }) => id === standId,
   ) as Stand;
 
-  const handleTakeStand = async () => {
-    try {
-      const response = await fetch(routes.api.stand(standId), {
-        method: 'PATCH',
-        headers: {
-          Authorization: `Basic ${user}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ takenBy: 'admin@test.ru' }),
-      });
+  const handleTakeStand = (id: number) => () => {
+    dispatch(apiTakeStand(id));
+    onClose();
+  };
 
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-
-      dispatch(takeStand('admin@test.ru', standId));
-    } catch (e) {
-      console.log(e);
-    }
+  const handleLeaveStand = (id: number) => () => {
+    dispatch(apiLeaveStand(id));
+    onClose();
   };
 
   return (
     <>
-      {!stand.takenBy && (
-        <Link to={`/stand/${standId}`}>
-          <Button
-            variant="contained"
-            color="success"
+      {!stand.takenBy ? (
+        <IconButton
+          sx={{
+            borderRadius: '10px',
+            boxShadow: 'none',
+          }}
+          onClick={handleTakeStand(standId)}
+        >
+          <LoginOutlined />
+        </IconButton>
+      ) : (
+        stand.takenBy === login && (
+          <IconButton
             sx={{
               borderRadius: '10px',
               boxShadow: 'none',
             }}
-            onClick={handleTakeStand}
+            onClick={handleLeaveStand(standId)}
           >
-            <AddOutlined />
-          </Button>
-        </Link>
+            <LogoutOutlined />
+          </IconButton>
+        )
       )}
     </>
   );
 };
 
-const RenderCell: FunctionComponent<{ params: GridCellParams }> = ({
-  params,
-}) => {
+const RenderCell: FC<{ params: GridCellParams }> = ({ params }) => {
   const theme = useTheme();
   return (
     <span
@@ -86,57 +81,60 @@ const RenderCell: FunctionComponent<{ params: GridCellParams }> = ({
   );
 };
 
-const columns: GridColDef[] = [
-  {
-    field: 'id',
-    headerAlign: 'center',
-    align: 'center',
-    headerName: 'ID',
-    width: 195,
-    renderCell: (params: GridCellParams) => {
-      const value = params.value as number;
-      return (
-        <Tooltip
-          title={`Name: ${value}, 
-        Option1: ${params.row.logi}, 
-        Option2: ${value}, 
-        Option3: ${value}, Option3: ${value}`}
-        >
-          <div>{value}</div>
-        </Tooltip>
-      );
+const Stands: FC<{ stands: Stand[]; onClose: () => void }> = ({
+  stands,
+  onClose,
+}) => {
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      headerAlign: 'center',
+      align: 'center',
+      headerName: 'ID',
+      width: 195,
+      renderCell: (params: GridCellParams) => {
+        const value = params.value as number;
+        return (
+          <Tooltip
+            title={`Name: ${value}, 
+          Option1: ${params.row.logi}, 
+          Option2: ${value}, 
+          Option3: ${value}, Option3: ${value}`}
+          >
+            <div>{value}</div>
+          </Tooltip>
+        );
+      },
     },
-  },
-  { field: 'host', headerName: 'Адрес', width: 250 },
-  {
-    field: 'status',
-    headerName: 'Статус',
-    width: 250,
-    renderCell: (params: GridCellParams) => <RenderCell params={params} />,
-  },
-  {
-    field: 'takenBy',
-    headerName: 'Пользователь',
-    width: 300,
-    renderCell: (params: GridCellParams) => (
-      <span style={{ color: params.value === '' ? 'green' : 'red' }}>
-        {params.value ? params.value.toString() : ''}
-      </span>
-    ),
-  },
-  {
-    field: 'Connect',
-    headerName: 'Подключиться',
-    align: 'center',
-    headerAlign: 'center',
-    width: 195,
-    renderCell: (params: GridCellParams) => (
-      <RenderButtonCell params={params} />
-    ),
-  },
-];
+    { field: 'host', headerName: 'Адрес', width: 250 },
+    {
+      field: 'status',
+      headerName: 'Статус',
+      width: 250,
+      renderCell: (params: GridCellParams) => <RenderCell params={params} />,
+    },
+    {
+      field: 'takenBy',
+      headerName: 'Пользователь',
+      width: 300,
+      renderCell: (params: GridCellParams) => (
+        <span style={{ color: params.value === '' ? 'green' : 'red' }}>
+          {params.value ? params.value.toString() : ''}
+        </span>
+      ),
+    },
+    {
+      field: 'Connect',
+      headerName: 'Подключиться',
+      align: 'center',
+      headerAlign: 'center',
+      width: 195,
+      renderCell: (params: GridCellParams) => (
+        <RenderButtonCell params={params} onClose={onClose} />
+      ),
+    },
+  ];
 
-const Stands: FunctionComponent<{ stands: Stand[] }> = ({ stands }) => {
   return (
     <div
       style={{
@@ -155,6 +153,15 @@ const Stands: FunctionComponent<{ stands: Stand[] }> = ({ stands }) => {
           initialState={{
             pagination: {
               paginationModel: { page: 0, pageSize: 5 },
+            },
+            // сортировка по первому столбцу по возрастанию
+            sorting: {
+              sortModel: [
+                {
+                  field: 'id',
+                  sort: 'asc',
+                },
+              ],
             },
           }}
           pageSizeOptions={[5, 10]}
--- frontend/src/components/StandsModal.tsx
+++ frontend/src/components/StandsModal.tsx
@@ -3,26 +3,20 @@ import { useSelector } from 'react-redux';
 
 import { Box, Modal, Paper, Typography } from '@mui/material';
 
-import fetchStands from '../api/fetchStands';
 import { useAppDispatch } from '../hooks/useAppDispatch';
-import { useAuthContext } from '../hooks/useAuthContext';
-import { giveStands } from '../reducers/standsReducer';
 import { RootState } from '../store';
-import { ModalProps, Stand } from '../types';
+import { ModalProps } from '../types';
 import Stands from './Stands';
+import { apiGetStands } from '../store/stands';
 
 const StandsModal: FunctionComponent<ModalProps> = ({ open, onClose }) => {
-  const { user } = useAuthContext();
 
   const stands = useSelector((state: RootState) => state.stands.stands);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    (async () => {
-      const fetchedStands: Stand[] = await fetchStands(user!);
-      dispatch(giveStands(fetchedStands));
-    })();
-  }, [user, dispatch]);
+    dispatch(apiGetStands());
+  }, [dispatch]);
 
   return (
     <Modal open={open} onClose={onClose}>
@@ -35,7 +29,6 @@ const StandsModal: FunctionComponent<ModalProps> = ({ open, onClose }) => {
           padding: '10px',
         }}
       >
-        {/* TODO Добавить проверку на наличие стендов */}
         {!stands.length ? (
           <Box
             height={200}
@@ -66,7 +59,7 @@ const StandsModal: FunctionComponent<ModalProps> = ({ open, onClose }) => {
                 Список стендов
               </h1>
             </Box>
-            <Stands stands={stands} />
+            <Stands stands={stands} onClose={onClose} />
           </>
         )}
       </Paper>
