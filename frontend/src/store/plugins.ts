45f28ca17182ede227d5793f9b69cf23ff76b4d2
SKornya <kornyakovS@gmail.com>
2024-05-03 21:40:28 +0300

    change user string to object with login and header

--- frontend/src/store/plugins.ts
+++ frontend/src/store/plugins.ts
@@ -1,41 +1,39 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
 import { fetchPlugins } from '../api/fetchPlugins';
 import { Plugin } from '../types';
 import { getUser } from './utils';
 
-
 export const apiGetPlugins = createAsyncThunk(
-    'plugins/apiGetPlugins',
-    async () => {
-        return await fetchPlugins(getUser());
-    }
+  'plugins/apiGetPlugins',
+  async () => {
+    const { header } = getUser();
+    return header ? await fetchPlugins(header) : [];
+  },
 );
 
 //export const apiSavePlugin = createAsyncThunk() ??сохранение изменений плагина
-//export const apiCreatePlugin = createAsyncThunk() ??создание нового плагина 
-
-
+//export const apiCreatePlugin = createAsyncThunk() ??создание нового плагина
 
 const pluginsSlice = createSlice({
-    name: 'plugins',
-    initialState: {
-        plugins: [] as Plugin[],
-    },
-    reducers: {
-        //savePlugin
-        //createPlugin
-        //removePlugin?
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(apiGetPlugins.fulfilled, (state, action) => {
-                state.plugins = action.payload;
-            })
-            .addCase(apiGetPlugins.rejected, (_state, action) => {
-                alert(action.error.message);
-            })
-    }
-
-})
+  name: 'plugins',
+  initialState: {
+    plugins: [] as Plugin[],
+  },
+  reducers: {
+    //savePlugin
+    //createPlugin
+    //removePlugin?
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(apiGetPlugins.fulfilled, (state, action) => {
+        state.plugins = action.payload;
+      })
+      .addCase(apiGetPlugins.rejected, (_state, action) => {
+        alert(action.error.message);
+      });
+  },
+});
 
 export default pluginsSlice.reducer;
