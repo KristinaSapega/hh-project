01e89ed35d87e9af86bf081899a77ad181f61c4d
SKornya <kornyakovS@gmail.com>
2024-05-05 21:43:00 +0300

    stand jobs 1st iteration

--- frontend/src/components/History/History.tsx
+++ frontend/src/components/History/History.tsx
@@ -1,12 +1,19 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material';
-import { Box, IconButton, Typography } from '@mui/material';
+import { Box, IconButton, SelectChangeEvent, Typography } from '@mui/material';
 
 import { HistoryProps } from '../../types';
 import StandsList from './Jobs/StandsList';
+import Jobs from './Jobs/Jobs';
 
 const History: FC<HistoryProps> = ({ isVisible, setIsVisible }) => {
+  const [selectedStand, setSelectedStand] = useState<string>('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelectedStand(event.target.value);
+  };
+
   return (
     <Box>
       {isVisible ? (
@@ -28,8 +35,10 @@ const History: FC<HistoryProps> = ({ isVisible, setIsVisible }) => {
               <ArrowRightOutlined />
             </IconButton>
 
-            <StandsList />
+            <StandsList selectedStand={selectedStand} handleChange={handleChange} />
           </Box>
+
+          {selectedStand && <Jobs selectedStand={selectedStand} />}
         </Box>
       ) : (
         <Box
