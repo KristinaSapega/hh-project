c9bddc71ed76355a6771a74746a80d7ca9b0dde4
SKornya <kornyakovS@gmail.com>
2024-05-06 14:58:12 +0300

    change render order

--- frontend/src/components/FormGenerator.tsx
+++ frontend/src/components/FormGenerator.tsx
@@ -8,11 +8,9 @@ import {
 
 import { AddOutlined } from '@mui/icons-material';
 import {
-  Box,
-  // Checkbox,
+  Box, // Checkbox,
   // FormControlLabel,
-  IconButton,
-  // MenuItem,
+  IconButton, // MenuItem,
   // Select,
   // SelectChangeEvent,
   TextField,
@@ -36,7 +34,9 @@ const FormGenerator: FunctionComponent<FormGeneratorProps> = ({
   setFormsData,
 }) => {
   const { id, name, description, paramsSchema } = plugin;
-  const fields = paramsSchema && Object.entries(paramsSchema.properties.services.items.properties);
+  const fields =
+    paramsSchema &&
+    Object.entries(paramsSchema.properties.services.items.properties);
   const [formsCount, setFormsCount] = useState<number>(1);
 
   const handleInputChange =
@@ -97,88 +97,89 @@ const FormGenerator: FunctionComponent<FormGeneratorProps> = ({
         <Typography variant="h6">{name}</Typography>
         <Typography>{description}</Typography>
       </Box>
-      {[...Array(formsCount)].map((_, index) => (
-        <Box
-          key={index}
-          sx={{
-            margin: '20px 0',
-            padding: '30px',
-            pt: '10px',
-            borderRadius: '20px',
-            border: '1px solid rgba(255, 255, 255, 0.7)',
-          }}
-        >
-          {fields &&
-            fields.map(([field, { type }]) => {
-              switch (type) {
-                case 'string':
-                  return (
-                    <TextField
-                      key={field}
-                      label={field}
-                      placeholder={field}
-                      name={field}
-                      value={
-                        (formData[index] && formData[index][field]) || ''
-                      }
-                      onChange={handleInputChange(index)}
-                      fullWidth
-                      required
-                      sx={{ margin: '10px 0' }}
-                    />
-                  );
-                // case 'checkbox':
-                //   return (
-                //     <FormControlLabel
-                //       key={field.name}
-                //       control={
-                //         <Checkbox
-                //           checked={
-                //             !!(formData[index] && formData[index][field.name])
-                //           }
-                //           onChange={handleCheckboxChange(index)}
-                //           name={field.name}
-                //           required
-                //         />
-                //       }
-                //       label={field.placeholder}
-                //     />
-                //   );
-                // case 'select':
-                //   return (
-                //     <Select
-                //       key={field.name}
-                //       value={
-                //         (formData[index] &&
-                //           (formData[index][field.name] as string)) ||
-                //         ''
-                //       }
-                //       onChange={handleSelectChange(index)}
-                //       fullWidth
-                //       label={field.placeholder}
-                //       name={field.name}
-                //       required
-                //     >
-                //       {field.options?.map((option) => (
-                //         <MenuItem key={option} value={option}>
-                //           {option}
-                //         </MenuItem>
-                //       ))}
-                //     </Select>
-                //   );
-                default:
-                  return null;
-              }
-            })}
-        </Box>
-      ))}
-      {fields && (
-        <Tooltip title="Добавить еще одну таску">
-          <IconButton onClick={() => setFormsCount((prev) => prev + 1)}>
-            <AddOutlined />
-          </IconButton>
-        </Tooltip>
-      )}
+      {fields &&
+        [...Array(formsCount)].map((_, index) => (
+          <Box>
+            <Box
+              key={index}
+              sx={{
+                margin: '20px 0',
+                padding: '30px',
+                pt: '10px',
+                borderRadius: '20px',
+                border: '1px solid rgba(255, 255, 255, 0.7)',
+              }}
+            >
+              {fields.map(([field, { type }]) => {
+                switch (type) {
+                  case 'string':
+                    return (
+                      <TextField
+                        key={field}
+                        label={field}
+                        placeholder={field}
+                        name={field}
+                        value={
+                          (formData[index] && formData[index][field]) || ''
+                        }
+                        onChange={handleInputChange(index)}
+                        fullWidth
+                        required
+                        sx={{ margin: '10px 0' }}
+                      />
+                    );
+                  // case 'checkbox':
+                  //   return (
+                  //     <FormControlLabel
+                  //       key={field.name}
+                  //       control={
+                  //         <Checkbox
+                  //           checked={
+                  //             !!(formData[index] && formData[index][field.name])
+                  //           }
+                  //           onChange={handleCheckboxChange(index)}
+                  //           name={field.name}
+                  //           required
+                  //         />
+                  //       }
+                  //       label={field.placeholder}
+                  //     />
+                  //   );
+                  // case 'select':
+                  //   return (
+                  //     <Select
+                  //       key={field.name}
+                  //       value={
+                  //         (formData[index] &&
+                  //           (formData[index][field.name] as string)) ||
+                  //         ''
+                  //       }
+                  //       onChange={handleSelectChange(index)}
+                  //       fullWidth
+                  //       label={field.placeholder}
+                  //       name={field.name}
+                  //       required
+                  //     >
+                  //       {field.options?.map((option) => (
+                  //         <MenuItem key={option} value={option}>
+                  //           {option}
+                  //         </MenuItem>
+                  //       ))}
+                  //     </Select>
+                  //   );
+                  default:
+                    return null;
+                }
+              })}
+            </Box>
+
+            <Tooltip title="Добавить еще одну таску">
+              <IconButton onClick={() => setFormsCount((prev) => prev + 1)}>
+                <AddOutlined />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        ))}
     </Box>
   );
 };
