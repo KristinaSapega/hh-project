86aa6207fa23ca6952e466a7cbe5386370e1a13e
SKornya <kornyakovS@gmail.com>
2024-05-15 12:33:40 +0300

    add more errors handling

--- frontend/src/api/fetchStands.ts
+++ frontend/src/api/fetchStands.ts
@@ -2,21 +2,31 @@ import { BASE_BACKEND_URL, routes } from '../routes/routes';
 import { Stand } from '../types';
 
 export const fetchStands = async (user: string): Promise<Stand[]> => {
-  const response = await fetch(`${BASE_BACKEND_URL}${routes.api.stands}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Basic ${user}`,
-      'X-Requested-With': 'XMLHttpRequest',
-    },
-  });
-  if (!response.ok) {
-    throw new Error(
-      'Ошибка при получении списка стендов. Код ошибки: ' + response.status,
-    );
-  }
+  try {
+    const response = await fetch(`${BASE_BACKEND_URL}${routes.api.stands}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Basic ${user}`,
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(
+        'Ошибка при получении списка стендов. Код ошибки: ' + response.status,
+      );
+    }
 
-  const data: Stand[] = (await response.json()).stands;
-  return data;
+    const data: Stand[] = (await response.json()).stands;
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error('Ошибка сети при запросе стендов');
+    }
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw Error('Ошибка при запросе плагинов');
+  }
 };
 
 export default fetchStands;
